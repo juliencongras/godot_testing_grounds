50924e602025079586647db9e8825850641e1fe4
juliencongras <julien.congras@gmail.com>
2026-09-24 08:26:20 +0200

    feat: 2d action platformer init

--- FirstPerson3DDungeonCrawler/dungeon_crawler_movement.gd
+++ FirstPerson3DDungeonCrawler/dungeon_crawler_movement.gd
@@ -1,8 +1,11 @@
 extends Node3D
 
-var cell_size: int = 100
+var cell_size: int = 10
 var turn_rotation: int = 90
 var tween_rotation: Tween
+var tween_movement: Tween
+
+@onready var camera_3d: Camera3D = $Camera3D
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -10,5 +13,7 @@ func _process(delta: float) -> void:
 		if tween_rotation: tween_rotation.kill()
 		tween_rotation = create_tween()
 		tween_rotation.tween_property(self, "rotation_degrees", Vector3(0.0, rotation_degrees.y + turn_rotation * Input.get_axis("right", "left"), 0.0), 0.5)
-	if Input.is_action_just_pressed("down") or Input.is_action_just_pressed("up"):
-		pass
+	elif Input.is_action_just_pressed("down") or Input.is_action_just_pressed("up"):
+		if tween_movement: tween_movement.kill()
+		tween_movement = create_tween()
+		tween_movement.tween_property(self, "position", Vector3(0, 0, position.z + cell_size * Input.get_axis("up", "down")), 0.5)
